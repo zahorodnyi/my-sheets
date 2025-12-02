7c02fed135dc1f601a21267bd848db3d51b16285
Oleksandr Zahorodnyi <zahorodnyiol@gmail.com>
2025-12-02 02:01:28 +0200

    Implement always visile col and row feature

--- MySheets.UI/Views/MainWindow.axaml.cs
+++ MySheets.UI/Views/MainWindow.axaml.cs
@@ -1,11 +1,17 @@
-using Avalonia.Controls;
-
 namespace MySheets.UI.Views;
+
+using Avalonia;
+using Avalonia.Controls;
 
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        MainScrollViewer.ScrollChanged += (s, e) => {
+            ColHeaderScrollViewer.Offset = new Vector(MainScrollViewer.Offset.X, 0);
+            RowHeaderScrollViewer.Offset = new Vector(0, MainScrollViewer.Offset.Y);
+        };
     }
 }
